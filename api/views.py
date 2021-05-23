2e948aaee291d269f70b90e8dc383d6687dec151
최성민 <csm1165@naver.com>
2021-05-23 14:58:04 +0900

    Added Permission classes in every ViewSet

--- api/views.py
+++ api/views.py
@@ -5,6 +5,119 @@ from django_filters.rest_framework import DjangoFilterBackend, FilterSet, filter
 # from rest_framework.response import Response
 from rest_framework import viewsets  # , status
 import datetime as dt
+from django.utils import timezone
+from api.models import Profile, Post, Media, HashTag, PeopleTag
+from api.serializers import UserSerializer, ProfileSerializer, PostSerializer, MediaSerializer, HashTagSerializer, \
+    PeopleTagSerializer
+from api.permission import IsUserOrReadonly
+
+# User ViewSet
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Profile Filter & ViewSet
+
+class ProfileFilter(FilterSet):
+    nickname__icontains = filters.CharFilter(field_name='content', lookup_expr='icontains')
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'name', 'nickname', 'website', 'email', 'phone', 'birthday']
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = ProfileFilter
+    permission_classes = [IsUserOrReadonly]
+
+
+# Post Filter & ViewSet
+
+class PostFilter(FilterSet):
+    pub_date__gt = filters.DateFilter(field_name='pub_date', lookup_expr='gt')  # 입력된 날짜 이후에 게시된 Post 필터링
+    pub_date__lt = filters.DateFilter(field_name='pub_date', lookup_expr='lt')  # 입력된 날짜 이전에 게시된 Post 필터링
+    pub_date__range = filters.DateFromToRangeFilter(field_name='pub_date', lookup_expr='range')  # 입력된 기간 내 게시된 Post 필터링
+    content__icontains = filters.CharFilter(field_name='content',
+                                            lookup_expr='icontains')  # 입력된 값을 content field value에 갖고 있는 Post 필터링
+    pub_date__recent_12h = filters.BooleanFilter(field_name='pub_date', method='filter_recent_12h')
+
+    class Meta:
+        model = Post
+        fields = ['profile', 'pub_date', 'content', 'location']  # 해당 Field에 대해 lookup_expr = 'exact' 필터링
+
+    def filter_recent_12h(self, queryset, pub_date, value):
+        current_time = timezone.now()
+        ref_time = current_time - dt.timedelta(hours=12)
+        if value:
+            filtered_queryset = queryset.filter(pub_date__gt=ref_time)
+        else:
+            filtered_queryset = queryset.filter(pub_date__lt=ref_time)
+        return filtered_queryset
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = PostFilter
+    permission_classes = [IsUserOrReadonly]
+
+
+# Media Filter & ViewSet
+
+class MediaFilter(FilterSet):
+    class Meta:
+        model = Media
+        fields = ['post']
+
+
+class MediaViewSet(viewsets.ModelViewSet):
+    queryset = Media.objects.all()
+    serializer_class = MediaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = MediaFilter
+    permission_classes = [IsUserOrReadonly]
+
+
+# HashTag Filter & ViewSet
+
+class HashTagFilter(FilterSet):
+    name__icontains = filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    class Meta:
+        model = HashTag
+        fields = ['post', 'name']
+
+
+class HashTagViewSet(viewsets.ModelViewSet):
+    queryset = HashTag.objects.all()
+    serializer_class = HashTagSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = HashTagFilter
+    permission_classes = [IsUserOrReadonly]
+
+
+# PeopleTag Filter & ViewSet
+
+class PeopleTagFilter(FilterSet):
+    name__icontains = filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    class Meta:
+        model = PeopleTag
+        fields = ['media', 'name']
+
+
+class PeopleTagViewSet(viewsets.ModelViewSet):
+    queryset = PeopleTag.objects.all()
+    serializer_class = PeopleTagSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = PeopleTagFilter
+    permission_classes = [IsUserOrReadonly]
 
 # class UserList(APIView):
 #     def get(self, request, format=None):
