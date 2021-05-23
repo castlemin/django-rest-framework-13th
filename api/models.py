9839ee9a4eda619a6bb08eaf13ddc5c1c139257d
최성민 <csm1165@naver.com>
2021-05-23 09:54:49 +0900

    Profile: profile_image, Media: upload -저장 경로 수정

--- api/models.py
+++ api/models.py
@@ -2,17 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def profile_directory_path(instance, filename):
+    return 'user_{0}/profile_image/{1}'.format(instance.user.id, filename)
+
+
+def media_directory_path(instance, filename):
+    return 'user_{0}/upload_{1}//%Y/%m/%d/{2}'.format(instance.user.id, instance.post.pk, filename)
+
+
 class Profile(models.Model):  # 사용자 모델
 
     gender_choices = (
         ('male', 'male'), ('female', 'female'), ('custom', 'custom'), ('None', 'None'))  # Profile.gender의 선택 사항을 저장항 튜플
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # User 모델에서 OneToOne 확장
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')  # User 모델에서 OneToOne 확장
     name = models.CharField(max_length=100)  # 사용자의 이름, 중복가능, 글자수 제한 100
     nickname = models.CharField(max_length=30, blank=False, unique=True)  # 사용자의 닉네임, 글자수 제한 30, 필수입력, 중복 불가능
     intro = models.CharField(max_length=2200)  # 사용자의 자기소개, 글자수 제한 2200
-    profile_image = models.ImageField(upload_to='profile_images/', blank=True,
-                                      null=True)  # 사용자의 프로필사진, /media/profile_images/에 저장되고 미입력 가능
+    profile_image = models.ImageField(upload_to=profile_directory_path, blank=True,
+                                      null=True)  # 사용자의 프로필사진, /media/user_id/profile_images/에 저장되고 미입력 가능
     website = models.URLField(blank=True, null=True)  # 사용자의 웹사이트 url , 중복 가능
     email = models.EmailField(blank=True, null=True)  # 사용자의 이메일 주소 , 중복 가능
     phone = models.CharField(max_length=30, blank=False, unique=True)  # 사용자의 전화번호 , 필수입력, 중복 불가능
@@ -36,7 +44,8 @@ class Post(models.Model):  # 포스트 모델
 class Media(models.Model):  # 미디어 모델 (포스트에 들어가는 사진/동영상)
 
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='medias')  # Post 모델과 1:N 관계
-    upload = models.FileField(upload_to='posts', blank=False, unique=True)  # 업로드된 사진 및 동영상, 필수 입력, 중복 불가능
+    upload = models.FileField(upload_to=media_directory_path, blank=False,
+                              unique=True)  # 업로드된 사진 및 동영상, /media/user_id/upload_pk/년/월/일 에 저장, 필수 입력, 중복 불가능
     subs_text = models.CharField('substitutional text', max_length=2200)  # 사진/영상 별 대체 텍스트 입력, 글자수 제한 2200
 
 
